1bc1ac8fabc196a3b5f9c3cf51eaa8f5e53a71eb
HanSeongLee <jkhanseong@gmail.com>
2023-08-23 18:53:34 +0900

    ADD | fonts

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -1,8 +1,36 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { Bellefair, Barlow_Condensed, Barlow } from '@next/font/google';
+
+const bellefair = Bellefair({
+      weight: ['400'],
+      subsets: ['latin'],
+    },
+);
+
+const BarlowCondensed = Barlow_Condensed({
+      weight: ['400', '500'],
+      subsets: ['latin'],
+    },
+);
+
+const barlow = Barlow({
+        weight: ['400'],
+        subsets: ['latin'],
+    },
+);
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <>
+    <style jsx global>{`
+      :root {
+        --bellefair-font: ${bellefair.style.fontFamily};
+        --barlow-condensed-font: ${BarlowCondensed.style.fontFamily};
+        --barlow-font: ${barlow.style.fontFamily};-
+      }
+    `}</style>
+    <Component {...pageProps} />
+  </>;
 }
 
-export default MyApp
+export default MyApp;
